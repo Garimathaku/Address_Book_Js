8327535dc8eeb5941384c2112a06577552669468
Garimathaku <garimathakur397@gmail.com>
2025-03-04 22:18:55 +0530

    [Garima]Add.Added ability to count contacts

--- src/index.js
+++ src/index.js
@@ -16,15 +16,16 @@ try {
     console.log("All Contacts Before editing", addressBook.getAllContacts());
     console.log(addressBook.editContact("Ria", { address: "Seoni", city: "New York", phone: "9234567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
-    console.log(addressBook.deleteContact("Ria"));
+    console.log(addressBook.deleteContact("Rakesh"));
     console.log("All Contacts After Deleting:", addressBook.getAllContacts());
+    console.log("Total Contacts:", addressBook.countContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "auh", "sf", "abc", "In", "I", "100A01", "12345", "invalid-email"
+        "uyh", "sdfg", "abc", "In", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
