bd544d3e620579f7df25a87ed503cef6aad10efe
Garimathaku <garimathakur397@gmail.com>
2025-03-04 22:25:06 +0530

    [Garima]Add.Added duplicate

--- src/index.js
+++ src/index.js
@@ -16,16 +16,21 @@ try {
     console.log("All Contacts Before editing", addressBook.getAllContacts());
     console.log(addressBook.editContact("Ria", { address: "Seoni", city: "New York", phone: "9234567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
-    console.log(addressBook.deleteContact("Rakesh"));
+    console.log(addressBook.deleteContact("Ria"));
     console.log("All Contacts After Deleting:", addressBook.getAllContacts());
-    console.log("Total Contacts:", addressBook.countContacts());
+    console.log("Total Contacts:", addressBook.countContacts()); 
+
+    const duplicateContact = new Contact(
+        "Garima", "Thakur", "Bhopal", "India", "In", "100001", "9876543210", "garimathakur@gmail.com"
+    );
+    console.log(addressBook.addContact(duplicateContact)); 
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "uyh", "sdfg", "abc", "In", "I", "100A01", "12345", "invalid-email"
+        "jn", "sn", "abc", "In", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
