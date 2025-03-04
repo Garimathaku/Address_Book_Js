7d75e719fed6abf98c146925a56a2a1e42b0dad3
Garimathaku <garimathakur397@gmail.com>
2025-03-04 21:57:30 +0530

    [Garima]Add.Added ability to store multiple contact

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Garima", "Thakur", "Bhopal", "India", "In", "1111", "9876543210", "garimathakur@gmail.com"
+        "Garima", "Thakur", "Bhopal", "India", "In", "100001", "9876543210", "garimathakur@gmail.com"
     );
-    console.log(addressBook.addContact(contact1));
-    console.log(addressBook.getAllContacts());
+    console.log(addressBook.addContact(contact1)); 
+
+    const contact2 = new Contact(
+        "Ria", "Roy", "Bhopal", "India","In","1000000","9197979988","riaroy@gmail.com"
+    )
+    console.log(addressBook.addContact(contact2)); 
+    console.log("All Contacts", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "ga", "thk", "abc", "In", "I", "100A01", "12345", "invalid-email"
+        "uj", "son", "abc", "In", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
