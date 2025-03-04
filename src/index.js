e0c9e62a58c6183e1b9a6465114b0a6e04b72c6b
Garimathaku <garimathakur397@gmail.com>
2025-03-04 22:03:06 +0530

    [Garima]Add.Added ability to find and edit contact by name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Garima", "Thakur", "Bhopal", "India", "In", "100001", "9876543210", "garimathakur@gmail.com"
+        "Garima", "Thakur", "Bhopal", "India", "In", "11111", "9876543210", "garimathakur@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
@@ -13,14 +13,16 @@ try {
         "Ria", "Roy", "Bhopal", "India","In","1000000","9197979988","riaroy@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
-    console.log("All Contacts", addressBook.getAllContacts());
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "uj", "son", "abc", "In", "I", "100A01", "12345", "invalid-email"
+        "an", "son", "abc", "In", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
